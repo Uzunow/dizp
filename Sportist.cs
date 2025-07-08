c400d75dc235b61775cc13d4003447c7dca9389c
Uzunow <125346134+Uzunow@users.noreply.github.com>
2025-07-08 13:10:40 +0300

    Add files via upload

---
+++ Sportist.cs
@@ -0,0 +1,11 @@
+﻿namespace ginerweb.Models
+{
+    public class Sportist
+    {
+        public int Id { get; set; }
+        public string Ime { get; set; }
+        public string Otbor { get; set; }
+        public string Poziciya { get; set; }
+        public string SnimkaUrl { get; set; }
+    }
+}
---
+++ SportistController.cs
@@ -0,0 +1,29 @@
+﻿using ginerweb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ginerweb.Controllers
+{
+    public class SportistController
+    {
+        public IActionResult Index()
+        {
+            var sportist = new Sportist
+            {
+                Id = 1,
+                Ime = "Lionel Messi",
+                Otbor = "Inter Miami",
+                Poziciya = "Нападател",
+                SnimkaUrl = "/images/messi-banner.jpg"
+            };
+
+            return View(sportist);
+        }
+
+        private IActionResult View(Sportist sportist)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
+
